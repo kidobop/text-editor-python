0d5312672ab21e1b0587acdff551629b2c536921
aaqil <kidobop@gmail.com>
2024-04-30 15:11:08 +0530

    set the frame

--- texter.py
+++ texter.py
@@ -1,2 +1,14 @@
 import tkinter as tk
 
+def main():
+    window = tk.Tk()
+    window.title("Text Editor")
+    window.rowconfigure(0,minsize=400)
+    window.columnconfigure(0,minsize=500)
+
+    text_edit = tk.Text(window, font="Montserrat 18 italic")
+    text_edit.grid(row=0,column=1)
+
+    window.mainloop()
+
+main()
