d7fd3ccd351cc3620c84fb81e6eeaa18fcf47616
aaqil <kidobop@gmail.com>
2024-04-30 16:42:36 +0530

    added the buttons

--- texter.py
+++ texter.py
@@ -1,13 +1,35 @@
 import tkinter as tk
+from tkinter.filedialog import askopenfilename,asksaveasfilename
+
+def open_file(window,text_edit):
+    filepath = askopenfilename(filetypes=[("Text Files","*.txt")])
+
+    if not filepath:
+        return 
+    
+    text_edit.delete(1.0,tk.END)
+    
+
+def save_file():
+    pass
+
 
 def main():
     window = tk.Tk()
     window.title("Text Editor")
     window.rowconfigure(0,minsize=400)
-    window.columnconfigure(0,minsize=500)
+    window.columnconfigure(1,minsize=500)
 
     text_edit = tk.Text(window, font="Montserrat 18 italic")
     text_edit.grid(row=0,column=1)
+
+    frame = tk.Frame(window,relief=tk.RAISED,bd=2)
+    save_button = tk.Button(frame,text="Save",command=test)
+    open_button = tk.Button(frame,text="Open")
+
+    save_button.grid(row=0,column=0,padx=5,pady=5,sticky="ew")
+    open_button.grid(row=1,column=0,padx=5,sticky="ew")
+    frame.grid(row=0,column=0,sticky="ns")
 
     window.mainloop()
 
